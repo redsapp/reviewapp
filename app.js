de1c585431267e5cbffd915b05aa7a394709e73a
Redouane Bouzekri <bouzekri.redouane@gmail.com>
2021-04-13 23:49:39 +0000

    File structure and nodejs setup

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-var name = "Hello";
-console.log(name);
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+
+const HomeRoutes = require('./routes/HomeRoute');
+
+const { add } = require('lodash');
+const { render } = require('ejs');
+//Express app
+const app = express();
+
+// db url
+const dbURI = ''
+
+// mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology:true})
+// .then((result)=> app.listen(3000))
+// .catch((err) => console.log(err));
+// Register view engine
+app.listen(3000);
+app.set('view engine', 'ejs');
+
+//Midleware static files
+app.use(express.static('public'));
+// Midleware post request
+app.use(express.urlencoded({extended:true}))
+// first route
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+//res.send('<h1>Hello world</h1>')
+
+    res.render('HomePage',{title:'Home'});
+
+
+
+
+});
+
+
+app.use('/home', HomeRoutes);
+
+app.use((req,res) => {
+    res.status(404).render('404',{title:'404'})
+
+});
---
+++ controllers/HomeController.js
@@ -0,0 +1,18 @@
+const Home = require('../models/Home');
+
+const home_index = (req,res) =>{
+    res.render('HomePage', {title:'Home Page'});
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    home_index,
+}

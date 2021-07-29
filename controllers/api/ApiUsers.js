d670ef8ab90357c9d674fc11f1dea20b6645fd75
Redouane Bouzekri <bouzekri.redouane@gmail.com>
2021-07-29 17:09:04 +0100

    login

--- controllers/api/ApiUsers.js
+++ controllers/api/ApiUsers.js
@@ -13,7 +13,7 @@ const user_create = (req,res) =>{
             const newuser = new User(req.body);
             if(password){
           
-              newuser.password = bcrypt.hashSync(password, 10);
+              newuser.password = bcrypt.hashSync(password.toString(), 10);
             }
             newuser.save()
                .then((result)=>{
@@ -36,11 +36,43 @@ const user_create = (req,res) =>{
 }
 
 
+const user_login = (req,res) =>{
+    const username = req.body.username;
+    const password = req.body.password
+    User.findOne({username:username})
+    .then((user)=>{
+        if(user){
+            bcrypt.compare(password.toString(), user.password, function(err, results){
+                if(err){
+                    throw new Error(err)
+                 }
+                 if (results) {
+                    req.session.loggedin = true;
+                    return res.status(200).json({ msg: "Login success" })
+                } else {
+                    return res.status(401).json({ msg: "Invalid credencial" })
+                }
+               })
+        }else{
+            res.send("Username doesn't exist")
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+    })
 
+}
 
+const user_logout = (req,res)=>{
+    req.session.loggedin = false;
+    res.redirect('/login');
+  
+}
 module.exports = {
    
     user_create,
+    user_login,
+    user_logout
  
 
 }
